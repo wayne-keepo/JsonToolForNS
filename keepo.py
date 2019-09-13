836f04e60e8f5e0783806e51d041f158acd20a15
Dmitry Pimushkin <dmitrii.pimushkin@netcracker.com>
2019-09-13 19:46:18 +0400

    q

--- keepo.py
+++ keepo.py
@@ -4,7 +4,6 @@ import os
 import json
 from glob import glob
 from jsonpath_rw import jsonpath, parse
-from itertools import ifilter
 
 FOR_RESULT_DOC_ADDITIONAL_PARAMS = 'additionalParams'
 FOR_RESULT_DOC_NESTED_ADDITIONAL_PARAMS = 'nestedAdditionalParams'
@@ -14,6 +13,7 @@ ADDITIONAL_PARAM = 'additionalParam'
 ADDITIONAL_PARAMS_FOR_NS = 'additionalParamForNs'
 ADDITIONAL_PARAMS_FOR_VNF = 'additionalParamForVnf'
 ADDITIONAL_PARAMS_FOR_NESTED_NS = 'additionalParamForNestedNs'
+SAPS = 'saps'
 
 VNF_PROFILE = 'vnfProfileId'
 NS_PROFILE = 'nsProfileId'
@@ -29,8 +29,12 @@ def find_all_paths_to_json():
         if len(curJ) > 0:
             for jp in glob(address + JSON_TYPE_FILTER):
                 json_path_list.append(jp)
-    print "End searching. This jsons will be changed: \n" + str(json_path_list)
+    print "End searching. Candidats for transfiguration: \n" + str(json_path_list)
     return json_path_list
+
+
+def path_converter(path):
+    return str(path).replace('[', '').replace(']', '').split('.')
 
 
 def set_key_name_by_path(data, path, newName):
@@ -38,8 +42,17 @@ def set_key_name_by_path(data, path, newName):
         data.update({newName: data.pop(path[0])})
         return
     if len(path) == 2:
-        data[path[0]].update({newName: data[path[0]].pop(path[1])})
-        return
+        if type(data) is list:
+            idx = int(path[0])
+            tmp_obj = data[idx]  # obj under list
+            data.pop(idx)
+            data.insert(idx, dict({newName: tmp_obj[path[1]]}))
+            return
+        else:
+            data[path[0]].update({newName: data[path[0]].pop(path[1])})
+            return
+    elif type(data) is list:
+        set_key_name_by_path(data[int(path.pop(0))], path, newName)
     else:
         set_key_name_by_path(data[path.pop(0)], path, newName)
 
@@ -47,6 +60,8 @@ def set_key_name_by_path(data, path, newName):
 def get_value_by_path(data, path):
     if len(path) == 1:
         return data[path[0]]
+    elif type(data) is list:
+        return get_value_by_path(data[int(path.pop(0))], path)
     else:
         return get_value_by_path(data[path.pop(0)], path)
 
@@ -55,6 +70,8 @@ def set_value_by_path(data, path, target):
     if len(path) == 1:
         data[path[0]] = target
         return
+    elif type(data) is list:
+        set_value_by_path(data[int(path.pop(0))], path, target)
     else:
         set_value_by_path(data[path.pop(0)], path, target)
 
@@ -66,11 +83,13 @@ def delete_child_by_path(data, path):
     if len(path) == 2:
         data[path[0]].pop(path[1])
         return
+    elif type(data) is list:
+        delete_child_by_path(data[int(path.pop(0))], path)
     else:
         delete_child_by_path(data[path.pop(0)], path)
 
 
-def find_nested_params_for_ns_by_ns_path_lvl(path, entity_list):
+def find_params_for_ns_by_ns_path_lvl(path, entity_list):
     path_to_ns_lvl = ''.join(str(path).split('.')[:-1])
     for entity_dict in entity_list:
         for entity_path, entity_value in entity_dict.items():
@@ -103,8 +122,9 @@ def create_vnf(data):
 
 def extract_vnfs(data):
     vnfs = list()
-    for item in data:
-        vnfs.append(create_vnf(item))
+    if data is not None:
+        for item in data:
+            vnfs.append(create_vnf(item))
     return vnfs
 
 
@@ -119,9 +139,18 @@ def create_nested_ns(data):
 
 def extract_nested_nss(data):
     nested_nss = list()
-    for item in data:
-        nested_nss.append(create_nested_ns(item))
+    if data is not None:
+        for item in data:
+            nested_nss.append(create_nested_ns(item))
     return nested_nss
+
+
+def check_invalid_json(data):
+    inv_path = 'instantiateNses'
+    inv_result = [match.full_path for match in parse('$..%s' % inv_path).find(data)]
+    if len(inv_result) > 0:
+        return True
+    return False
 
 
 # find all NS -> [0:n] transf into root FOR_RESULT_DOC_ADDITIONAL_PARAMS >
@@ -130,15 +159,12 @@ def extract_nested_nss(data):
 # > find Nested NS and repeat operation as for NS
 def transfiguration(data):
     # 1. find all NS:
-    print "##########################################################"
+    if check_invalid_json(data):
+        return dict()
+    print "---------------------"
     print "START TRANSFIGURATION..."
-    print "---------------------"
-    print "Start searching all NS..."
-    root_ns_list = [{match.full_path: match.value} for match in parse('$..%s' % ADDITIONAL_PARAMS_FOR_NS).find(data)]
-    print root_ns_list
-    print "End searching all NS."
-    print "---------------------"
     result = dict()
+    root_ns_list = [{match.full_path: match.value} for match in parse('$..%s' % ADDITIONAL_PARAMS_FOR_NS).find(data)]
     # 2. walk by list of NS dict for extract pair {pathToNs:NsValue}
     for root_ns_dict in root_ns_list:
         # 3. walk by NS dict for start creating root AP and nested AP for every NS
@@ -149,104 +175,77 @@ def transfiguration(data):
             root_vnf_list = [{match.full_path: match.value} for match in
                              parse('$..%s' % ADDITIONAL_PARAMS_FOR_VNF).find(data)]
             # 5. extract VNF AP which are only at the NS AP level
-            root_vnf_ = find_nested_params_for_ns_by_ns_path_lvl(paths, root_vnf_list)
+            root_vnf_ = find_params_for_ns_by_ns_path_lvl(paths, root_vnf_list)
 
             # 6. find nested NS AP
             nested_nss_list = [{match.full_path: match.value} for match in
                                parse('$..%s' % ADDITIONAL_PARAMS_FOR_NESTED_NS).find(data)]
             # 7. extract nested NS AP which are only at the NS level
-            nested_nss_ = find_nested_params_for_ns_by_ns_path_lvl(paths, nested_nss_list)
+            nested_nss_ = find_params_for_ns_by_ns_path_lvl(paths, nested_nss_list)
             # 8. create nested params for nns and vnf && fill nested ap && delete nns and vnf from source json
             if root_vnf_ is not None:
-                print "---------------------"
-                print "\tStart extract>convert>delete VNF..."
-                print "\tVNF paths: %s" % str(root_vnf_.keys()[0])
+                print "\tFound VNF paths: %s" % str(root_vnf_.keys()[0])
                 ext_nested_vnfs = extract_vnfs(root_vnf_.values()[0])
                 tmp_root_ap[FOR_RESULT_DOC_NESTED_ADDITIONAL_PARAMS].extend(ext_nested_vnfs)
-                delete_child_by_path(data, str(root_vnf_.keys()[0]).split('.'))
-                print "\tEnd extract>convert>delete VNF..."
-                print "---------------------"
+                tmp_path_vnf = path_converter(root_vnf_.keys()[0])
+                delete_child_by_path(data, tmp_path_vnf)
+
             if nested_nss_ is not None:
-                print "---------------------"
-                print "\tStart extract>convert>delete nested NS..."
-                print "\tNested NS paths: %s" % str(nested_nss_.keys()[0])
+                print "\tFound Nested NS paths: %s" % str(nested_nss_.keys()[0])
                 ext_nested_nss = extract_nested_nss(nested_nss_.values()[0])
                 tmp_root_ap[FOR_RESULT_DOC_NESTED_ADDITIONAL_PARAMS].extend(ext_nested_nss)
-                delete_child_by_path(data, str(nested_nss_.keys()[0]).split('.'))
-                print "\tEnd extract>convert>delete nested NS..."
-                print "---------------------"
+                tmp_path_nss = path_converter(nested_nss_.keys()[0])
+                delete_child_by_path(data, tmp_path_nss)
 
-            print "Set key name by path: %s " % str(paths)
-            set_value_by_path(data, str(paths).split('.'), tmp_root_ap)
+            # 9. take SAPSs
+            root_saps_list = [{match.full_path: match.value} for match in parse('$..%s' % SAPS).find(data)]
+            root_saps_ = find_params_for_ns_by_ns_path_lvl(paths, root_saps_list)
+            if root_saps_ is not None:
+                print "\t Found SAPSs: %s" % str(root_saps_.keys())
+                tmp_path_saps = path_converter(root_saps_.keys()[0])
+                tmp_saps_value = get_value_by_path(data, tmp_path_saps)
+                tmp_root_ap.update({SAPS: tmp_saps_value})
+                tmp_path_saps = path_converter(root_saps_.keys()[0])
+                delete_child_by_path(data, tmp_path_saps)
+
+            # 10. change name
+            print "Set key name %s on %s" % (ADDITIONAL_PARAMS_FOR_NS, FOR_RESULT_DOC_ADDITIONAL_PARAMS)
+            tmp_path_ns = str(paths).replace('[', '').replace(']', '').split('.')
+            set_value_by_path(data, tmp_path_ns, tmp_root_ap)
 
             result.update({paths: tmp_root_ap})
             print "End transfiguration for NS by path: %s" % str(paths)
 
-    print "Result transfiguration: %s" % str(result)
+    print "Result transfiguration: \n%s" % str(result)
     print "END TRANSFIGURATION."
     print "##########################################################"
     return result
 
 
-# $.attributes.instance.additionalParamForNS{}
-
-def search(data, key, path='$', tmpPath=''):
-    # pv = {}
-    for k, value in data.items():
-        if type(value) is dict and value.get(key):
-            path += tmpPath + '.%s' % k
-        if k == key:
-            pv = {}
-            path += '.%s' % k
-            pv['path'] = path
-            pv['value'] = data.pop(k)
-            return pv
-        elif type(value) is dict:
-            tmpPath += '.%s' % k
-            # проброс значения в стеке вызова
-            pv = search(value, key, path, tmpPath)
-            # условие необх для того, чтобы рекурсия дошла до нужного места
-            if type(pv) is dict and len(pv.items()) > 0:
-                return pv
-
-
-def start(data):
-    root = dict()
-    pathForNS = ''
-    search_ns = search(data, ADDITIONAL_PARAMS_FOR_NS, pathForNS)
-    # pathForNS = search_ns.get('path')
-    ap_ns = search_ns.get('value')
-    if ap_ns.get(ADDITIONAL_PARAM):
-        root[ADDITIONAL_PARAM] = ap_ns.pop(ADDITIONAL_PARAM)
-    root[ADDITIONAL_PARAM].update(ap_ns)
-
-    search_vnf = search(data, ADDITIONAL_PARAMS_FOR_VNF)
-    ap_ns[FOR_RESULT_DOC_NESTED_ADDITIONAL_PARAMS] = extract_vnfs(search_vnf.get('value'))
-    root[ADDITIONAL_PARAM].update(ap_ns)
-
-    # by path from search_result for APNS added root AP in document
-
-    return root
-
-
 def by_files(json_path_list):
     for pathToJson in json_path_list:
         print "Start work with json file: " + pathToJson
-        with open(pathToJson) as json_file:
+        with open(pathToJson, 'r+') as json_file:
             jData = json.load(json_file)
             work_test = transfiguration(jData)
             for path, ap in work_test.items():
-                set_key_name_by_path(jData, str(path).split('.'), FOR_RESULT_DOC_ADDITIONAL_PARAMS)
-
-            json_file.seek(0)  # rewind
+                tmp_path = path_converter(path)
+                set_key_name_by_path(jData, tmp_path, FOR_RESULT_DOC_ADDITIONAL_PARAMS)
+            json_file.seek(0)
             json.dump(jData, json_file)
             json_file.truncate()
 
         print "End work with json file."
 
+def get_argv():
+    return
 
 def run():
-    jsonPathList = find_all_paths_to_json()
-    by_files(jsonPathList)
+    if len(sys.argv) > 1:
+        return
+    else:
+        jsonPathList = find_all_paths_to_json()
+        by_files(jsonPathList)
+
 
 run()
